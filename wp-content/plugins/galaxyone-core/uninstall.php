828174be8e3829c35d67c86fa4d4437ae375404a
LALIT KISHORE J <jeevatham@gmail.com>
2026-07-23 23:12:04 +0530

    Fix uninstall cleanup

--- wp-content/plugins/galaxyone-core/uninstall.php
+++ wp-content/plugins/galaxyone-core/uninstall.php
@@ -7,12 +7,4 @@
 
 defined( 'WP_UNINSTALL_PLUGIN' ) || exit;
 
-$galaxyone_core_autoload_file = __DIR__ . '/vendor/autoload.php';
-
-if ( ! file_exists( $galaxyone_core_autoload_file ) ) {
-	return;
-}
-
-require_once $galaxyone_core_autoload_file;
-
-\GalaxyOne\Core\Database\SchemaManager::uninstall();
+delete_option( 'galaxyone_core_schema_version' );
