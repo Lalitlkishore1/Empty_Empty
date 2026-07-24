12cb4af1076cdfd0c376e57f45dab0bcfaffe0db
LALIT KISHORE J <jeevatham@gmail.com>
2026-07-24 11:17:54 +0530

    Expand GalaxyOne capability service

--- wp-content/plugins/galaxyone-core/app/Security/Capabilities.php
+++ wp-content/plugins/galaxyone-core/app/Security/Capabilities.php
@@ -10,11 +10,27 @@ namespace GalaxyOne\Core\Security;
 final class Capabilities {
 
 	/**
+	 * Capability required to manage GalaxyOne administration settings.
+	 *
+	 * @var string
+	 */
+	private const MANAGE_CAPABILITY = 'manage_woocommerce';
+
+	/**
+	 * Returns the capability required to manage GalaxyOne.
+	 *
+	 * @return string
+	 */
+	public static function get_manage_capability(): string {
+		return self::MANAGE_CAPABILITY;
+	}
+
+	/**
 	 * Determines whether the current user can manage GalaxyOne settings.
 	 *
 	 * @return bool
 	 */
 	public static function can_manage_galaxyone(): bool {
-		return current_user_can( 'manage_woocommerce' );
+		return current_user_can( self::get_manage_capability() );
 	}
 }
