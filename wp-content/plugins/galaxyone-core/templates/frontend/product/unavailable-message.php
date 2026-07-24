7cdf835d664f61d7d5bbf0b82496a238f88c5ce5
LALIT KISHORE J <jeevatham@gmail.com>
2026-07-24 14:50:11 +0530

    Create product unavailable message

---
+++ wp-content/plugins/galaxyone-core/templates/frontend/product/unavailable-message.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Product unavailable message.
+ *
+ * @package GalaxyOne\Core
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+<p class="stock out-of-stock galaxyone-product-unavailable" role="status">
+	<?php esc_html_e( 'Currently unavailable.', 'galaxyone-core' ); ?>
+</p>
