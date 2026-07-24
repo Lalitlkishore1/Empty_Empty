63f2c73d0ec492863c1b83ce784f3bdbbafa1d24
LALIT KISHORE J <jeevatham@gmail.com>
2026-07-24 22:51:11 +0530

    Update uninstall for Phase 7

--- wp-content/plugins/galaxyone-core/uninstall.php
+++ wp-content/plugins/galaxyone-core/uninstall.php
@@ -9,12 +9,14 @@ defined( 'WP_UNINSTALL_PLUGIN' ) || exit;
 
 global $wpdb;
 
-$galaxyone_delivery_reservations_table  = $wpdb->prefix . 'galaxy_delivery_reservations';
-$galaxyone_delivery_capacities_table    = $wpdb->prefix . 'galaxy_delivery_capacities';
-$galaxyone_delivery_rules_table         = $wpdb->prefix . 'galaxy_delivery_rules';
-$galaxyone_flower_daily_price_table     = $wpdb->prefix . 'galaxy_flower_daily_prices';
-$galaxyone_activity_log_table           = $wpdb->prefix . 'galaxy_activity_logs';
+$galaxyone_offer_campaigns_table       = $wpdb->prefix . 'galaxy_offer_campaigns';
+$galaxyone_delivery_reservations_table = $wpdb->prefix . 'galaxy_delivery_reservations';
+$galaxyone_delivery_capacities_table   = $wpdb->prefix . 'galaxy_delivery_capacities';
+$galaxyone_delivery_rules_table        = $wpdb->prefix . 'galaxy_delivery_rules';
+$galaxyone_flower_daily_price_table    = $wpdb->prefix . 'galaxy_flower_daily_prices';
+$galaxyone_activity_log_table          = $wpdb->prefix . 'galaxy_activity_logs';
 
+$wpdb->query( "DROP TABLE IF EXISTS {$galaxyone_offer_campaigns_table}" ); // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
 $wpdb->query( "DROP TABLE IF EXISTS {$galaxyone_delivery_reservations_table}" ); // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
 $wpdb->query( "DROP TABLE IF EXISTS {$galaxyone_delivery_capacities_table}" ); // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
 $wpdb->query( "DROP TABLE IF EXISTS {$galaxyone_delivery_rules_table}" ); // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
