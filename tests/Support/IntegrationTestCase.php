9182871dd0d7589ba5ace80d3289813bb181efec
LALIT KISHORE J <jeevatham@gmail.com>
2026-07-26 13:56:27 +0530

    Add integration test base class

---
+++ tests/Support/IntegrationTestCase.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Integration test base class.
+ *
+ * @package GalaxyOne\Tests\Support
+ */
+
+declare(strict_types=1);
+
+namespace GalaxyOne\Tests\Support;
+
+abstract class IntegrationTestCase extends \WP_UnitTestCase {
+}
